f583f762402fea9b0bc10f077ec431abab676bbe
Igor Lemeshkin <igorlem@icloud.com>
2023-01-16 00:21:44 +0400

    App component with global styles

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import '../i18n/config';
+
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Main, Error } from './screens';
+import { GlobalStyleWrapper } from './components';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+    errorElement: <Error />,
+  },
+]);
+
+const App: React.FC = () => (
+  <GlobalStyleWrapper>
+    <RouterProvider router={router} />
+  </GlobalStyleWrapper>
+);
+
+export default App;
