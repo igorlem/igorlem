108ffc2eaa911f9c1e5885deec391878dd350b86
Igor Lemeshkin <igorlem@icloud.com>
2023-01-16 02:20:22 +0400

    favicon webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
   target: 'web',
@@ -28,6 +29,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'index.html'),
+    }),
+    new FaviconsWebpackPlugin({
+      logo: path.resolve(__dirname, 'src', 'assets', 'logo.svg'),
     }),
   ],
 };
