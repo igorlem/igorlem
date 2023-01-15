3f0d94ef5705ca248eea7c25365662c45d3f2ced
Igor Lemeshkin <igorlem@icloud.com>
2023-01-16 02:23:53 +0400

    complete i18n setup

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,12 +1,13 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 // Resources
-import main from './en/main.json';
+import intro from './en/intro.json';
 import work from './en/work.json';
 
 i18next.use(initReactI18next).init({
   resources: {
-    en: { main, work },
+    en: { intro, work },
   },
   debug: true,
+  fallbackLng: 'en',
 });
